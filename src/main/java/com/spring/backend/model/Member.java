bcd5a5422b32d8f5faf8d9bcc06a5baf50d35f99
gang-gyeonghun <rudgns2040@naver.com>
2025-05-02 19:16:29 +0900

    엔티티 카멜케이스 변환

--- src/main/java/com/spring/backend/model/Member.java
+++ src/main/java/com/spring/backend/model/Member.java
@@ -1,126 +1,47 @@
 package com.spring.backend.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Date;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString(exclude = {"chkId", "chkDate"})
 @Entity
 @Table(name = "MEMBER")
 public class Member {
 
     @Id
     @Column(name = "MEMBER_ID")
-    private String MEMBER_ID;   // 회원ID
+    private String memberId;   // 회원ID
 
     @Column(name = "MEMBER_PWD")
-    private String MEMBER_PWD;  // 비밀번호
+    private String memberPwd;  // 비밀번호
 
     @Column(name = "NAME")
-    private String NAME;        // 이름
+    private String name;        // 이름
 
     @Column(name = "TEL_NO")
-    private String TEL_NO;      // 연락처
+    private String telNo;      // 연락처
 
     @Column(name = "EMAIL")
-    private String EMAIL;       // 이메일주소
+    private String email;       // 이메일주소
 
     @Column(name = "DELETE_YN")
-    private String DELETE_YN;   // 삭제여부
+    private String deleteYn;   // 삭제여부
 
     @Column(name = "JOIN_DATE")
     @Temporal(TemporalType.TIMESTAMP)
-    private Date JOIN_DATE;     // 가입일자
+    private Date joinDate;     // 가입일자
 
     @Column(name = "CHK_ID")
-    private String CHK_ID;      // 최종작업자
+    private String chkId;      // 최종작업자
 
     @Temporal(TemporalType.TIMESTAMP)
-    private Date CHK_DATE;      // 최종작업시간
+    private Date chkDate;      // 최종작업시간
 
-    public String getMEMBER_ID() {
-        return MEMBER_ID;
-    }
-
-    public void setMEMBER_ID(String MEMBER_ID) {
-        this.MEMBER_ID = MEMBER_ID;
-    }
-
-    public String getMEMBER_PWD() {
-        return MEMBER_PWD;
-    }
-
-    public void setMEMBER_PWD(String MEMBER_PWD) {
-        this.MEMBER_PWD = MEMBER_PWD;
-    }
-
-    public String getNAME() {
-        return NAME;
-    }
-
-    public void setNAME(String NAME) {
-        this.NAME = NAME;
-    }
-
-    public String getTEL_NO() {
-        return TEL_NO;
-    }
-
-    public void setTEL_NO(String TEL_NO) {
-        this.TEL_NO = TEL_NO;
-    }
-
-    public String getEMAIL() {
-        return EMAIL;
-    }
-
-    public void setEMAIL(String EMAIL) {
-        this.EMAIL = EMAIL;
-    }
-
-    public String getDELETE_YN() {
-        return DELETE_YN;
-    }
-
-    public void setDELETE_YN(String DELETE_YN) {
-        this.DELETE_YN = DELETE_YN;
-    }
-
-    public Date getJOIN_DATE() {
-        return JOIN_DATE;
-    }
-
-    public void setJOIN_DATE(Date JOIN_DATE) {
-        this.JOIN_DATE = JOIN_DATE;
-    }
-
-    public String getCHK_ID() {
-        return CHK_ID;
-    }
-
-    public void setCHK_ID(String CHK_ID) {
-        this.CHK_ID = CHK_ID;
-    }
-
-    public Date getCHK_DATE() {
-        return CHK_DATE;
-    }
-
-    public void setCHK_DATE(Date CHK_DATE) {
-        this.CHK_DATE = CHK_DATE;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "MEMBER_ID='" + MEMBER_ID + '\'' +
-                ", MEMBER_PWD='" + MEMBER_PWD + '\'' +
-                ", NAME='" + NAME + '\'' +
-                ", TEL_NO='" + TEL_NO + '\'' +
-                ", EMAIL='" + EMAIL + '\'' +
-                ", DELETE_YN='" + DELETE_YN + '\'' +
-                ", JOIN_DATE=" + JOIN_DATE +
-                ", CHK_ID='" + CHK_ID + '\'' +
-                ", CHK_DATE=" + CHK_DATE +
-                '}';
-    }
 }
--- src/main/java/com/spring/backend/model/Week_Goal.java
+++ src/main/java/com/spring/backend/model/Week_Goal.java
@@ -1,149 +1,52 @@
 package com.spring.backend.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Date;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString(exclude = {"chkId", "chkDate"})
 @Entity
 @Table(name = "WEEK_GOAL")
 public class Week_Goal {
 
     @Id @Column(name = "TEAM_ID")
-    private String TEAM_ID;         // 팀ID
+    private String teamId;         // 팀ID
 
     @Id @Column(name = "WEEK")
-    private int WEEK;               // 주차
+    private int week;               // 주차
 
     @Id @Column(name = "MEMBER_ID")
-    private String MEMBER_ID;       // 회원ID
+    private String memberId;       // 회원ID
 
     @Id @Column(name = "GOAL_NO")
-    private int GOAL_NO;            // 목표번호
+    private int goalNo;            // 목표번호
 
     @Column(name = "GOAL")
-    private String GOAL;                // 목표내용
+    private String goal;                // 목표내용
 
     @Column(name = "ORDER_SEQ")
-    private int ORDER_SEQ;              // 정렬순서
+    private int orderSeq;              // 정렬순서
 
     @Column(name = "MAIN_GOAL_YN")
-    private String MAIN_GOAL_YN;        // 메인목표여부
+    private String mainGoalYn;        // 메인목표여부
 
     @Column(name = "ACHIEVE_STATUS_CD")
-    private String ACHIEVE_STATUS_CD;   // 달성상태코드
+    private String achieveStatusCd;   // 달성상태코드
 
     @Column(name = "FEEDBACK")
-    private String FEEDBACK;            // 피드백내용
+    private String feedback;            // 피드백내용
 
     @Column(name = "CHK_ID")
-    private String CHK_ID;              // 최종작업자
+    private String chkId;              // 최종작업자
 
     @Column(name = "CHK_DATE")
     @Temporal(TemporalType.TIMESTAMP)
-    private Date CHK_DATE;              // 최종작업시간
+    private Date chkDate;              // 최종작업시간
 
-    public String getTEAM_ID() {
-        return TEAM_ID;
-    }
-
-    public void setTEAM_ID(String TEAM_ID) {
-        this.TEAM_ID = TEAM_ID;
-    }
-
-    public int getWEEK() {
-        return WEEK;
-    }
-
-    public void setWEEK(int WEEK) {
-        this.WEEK = WEEK;
-    }
-
-    public String getMEMBER_ID() {
-        return MEMBER_ID;
-    }
-
-    public void setMEMBER_ID(String MEMBER_ID) {
-        this.MEMBER_ID = MEMBER_ID;
-    }
-
-    public int getGOAL_NO() {
-        return GOAL_NO;
-    }
-
-    public void setGOAL_NO(int GOAL_NO) {
-        this.GOAL_NO = GOAL_NO;
-    }
-
-    public String getGOAL() {
-        return GOAL;
-    }
-
-    public void setGOAL(String GOAL) {
-        this.GOAL = GOAL;
-    }
-
-    public int getORDER_SEQ() {
-        return ORDER_SEQ;
-    }
-
-    public void setORDER_SEQ(int ORDER_SEQ) {
-        this.ORDER_SEQ = ORDER_SEQ;
-    }
-
-    public String getMAIN_GOAL_YN() {
-        return MAIN_GOAL_YN;
-    }
-
-    public void setMAIN_GOAL_YN(String MAIN_GOAL_YN) {
-        this.MAIN_GOAL_YN = MAIN_GOAL_YN;
-    }
-
-    public String getACHIEVE_STATUS_CD() {
-        return ACHIEVE_STATUS_CD;
-    }
-
-    public void setACHIEVE_STATUS_CD(String ACHIEVE_STATUS_CD) {
-        this.ACHIEVE_STATUS_CD = ACHIEVE_STATUS_CD;
-    }
-
-    public String getFEEDBACK() {
-        return FEEDBACK;
-    }
-
-    public void setFEEDBACK(String FEEDBACK) {
-        this.FEEDBACK = FEEDBACK;
-    }
-
-    public String getCHK_ID() {
-        return CHK_ID;
-    }
-
-    public void setCHK_ID(String CHK_ID) {
-        this.CHK_ID = CHK_ID;
-    }
-
-    public Date getCHK_DATE() {
-        return CHK_DATE;
-    }
-
-    public void setCHK_DATE(Date CHK_DATE) {
-        this.CHK_DATE = CHK_DATE;
-    }
-
-    @Override
-    public String toString() {
-        return "Week_Goal{" +
-                "TEAM_ID='" + TEAM_ID + '\'' +
-                ", WEEK=" + WEEK +
-                ", MEMBER_ID='" + MEMBER_ID + '\'' +
-                ", GOAL_NO=" + GOAL_NO +
-                ", GOAL='" + GOAL + '\'' +
-                ", ORDER_SEQ=" + ORDER_SEQ +
-                ", MAIN_GOAL_YN='" + MAIN_GOAL_YN + '\'' +
-                ", ACHIEVE_STATUS_CD='" + ACHIEVE_STATUS_CD + '\'' +
-                ", FEEDBACK='" + FEEDBACK + '\'' +
-                ", CHK_ID='" + CHK_ID + '\'' +
-                ", CHK_DATE=" + CHK_DATE +
-                '}';
-    }
 }
