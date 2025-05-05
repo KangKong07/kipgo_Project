f49d7e78ff0580e5ff8d0923aaeef15c0706d8ae
gang-gyeonghun <rudgns2040@naver.com>
2025-05-05 22:15:53 +0900

    Embeddable Repository 파일 삭제

--- src/main/java/com/spring/backend/repository/WeekIdRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.backend.repository;
-
-import com.spring.backend.model.WeekId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeekIdRepository extends JpaRepository<WeekId, String> {
-}
