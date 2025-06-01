99b988cb726b9758b971e04272a7d9e01a06291e
hyeonyeong Nam <hyeonyeongn95@gmail.com>
2025-06-01 21:23:08 +0900

    refactor : 회원가입 api auth로 이동

--- src/main/java/com/spring/backend/controller/AuthController.java
+++ src/main/java/com/spring/backend/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.spring.backend.controller;
 
+import com.spring.backend.common.response.ApiResponse;
 import com.spring.backend.dto.request.LoginRequest;
+import com.spring.backend.dto.request.RegisterRequest;
 import com.spring.backend.dto.response.LoginResponse;
 import com.spring.backend.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,10 +21,38 @@ public class AuthController {
 
     private final AuthService authService;
 
+
+    /**
+     * 로그인
+     * @param request 로그인 요청 정보
+     * @return 로그인 응답 정보 (JWT 토큰 포함)
+     */
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         LoginResponse response = authService.login(request);
         return ResponseEntity.ok(response);
+    }
+
+
+    /**
+     * 회원가입
+     * @param reqMember 회원가입 요청 정보
+     * @return ApiResponse 처리상태, 성공 또는 실패 메시지
+     */
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<?>> registerMember(@RequestBody RegisterRequest reqMember) {
+        try {
+            authService.registerMember(reqMember);    // 회원가입 처리 호출
+            return ResponseEntity.ok(ApiResponse.success("회원가입 성공"));
+        } catch (IllegalStateException e) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(ApiResponse.error(e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(ApiResponse.error("회원가입 중 알 수 없는 오류 발생"));
+        }
     }
 
 
--- src/main/java/com/spring/backend/service/AuthService.java
+++ src/main/java/com/spring/backend/service/AuthService.java
@@ -2,12 +2,15 @@ package com.spring.backend.service;
 
 import com.spring.backend.common.util.JwtUtil;
 import com.spring.backend.dto.request.LoginRequest;
+import com.spring.backend.dto.request.RegisterRequest;
 import com.spring.backend.dto.response.LoginResponse;
 import com.spring.backend.model.Member;
 import com.spring.backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +20,11 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
+    /**
+     * 로그인
+     * @param request 로그인 요청 정보
+     * @return 로그인 응답 정보 (JWT 토큰 포함)
+     */
     public LoginResponse login(LoginRequest request) {
 
         // 1. 회원ID로 회원 정보 조회(+존재여부체크)
@@ -37,5 +45,42 @@ public class AuthService {
         String token = jwtUtil.generateToken(member.getMemberId());
 
         return new LoginResponse(token, member);
+    }
+
+
+    /**
+     * 회원가입
+     * @param reqMember 회원가입 요청 정보
+     */
+    public void registerMember(RegisterRequest reqMember) {
+
+        // 1. 회원ID 중복 체크
+        boolean exists = memberRepository.existsByMemberId(reqMember.getMemberId());
+
+        if (exists) {
+            throw new IllegalStateException("이미 존재하는 사용자입니다.");
+        }
+
+        // 2. 비밀번호 유효성 검사
+        // ** 비밀번호 8자리 이상, 대소문자와 특수문자가 섞였는지 체크
+        if (!reqMember.getMemberPwd().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{8,}$")) {
+            throw new IllegalStateException("비밀번호는 8자 이상이며, 대문자, 소문자, 특수문자가 각각 하나 이상 포함되어야 합니다.");
+        }
+
+        // 3. 회원정보 저장
+        Member member = Member.builder()
+                .memberId(reqMember.getMemberId())
+                .memberPwd(passwordEncoder.encode(reqMember.getMemberPwd()))
+                .name(reqMember.getName())
+                .telNo(reqMember.getTelNo())
+                .email(reqMember.getEmail())
+                .deleteYn("N")
+                .joinDate(new Date())
+                .chkId(reqMember.getMemberId())
+                .chkDate(new Date())
+                .mainTeamId("")
+                .build();
+
+        memberRepository.save(member);
     }
 }
